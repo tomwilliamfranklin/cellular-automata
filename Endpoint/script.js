3edce8988dd6572cfef36c9229db7c4a7bd56a07
Tom <tomwilliamfranklin@gmail.com>
2020-01-04 22:07:37 +0000

    Added new algorithm for cells fighting. A final nation can now actually win (except for nations on other landmasses...

--- Endpoint/script.js
+++ Endpoint/script.js
@@ -2,7 +2,7 @@
 
 const w=540; //Size of Grid, recommend equal height and width
 const h=360;
-let frames = 35; 
+let frames = 30; 
 let teams = 20; 
 //green = 4,255,0
 //blue = 26,0,255
@@ -68,16 +68,16 @@ function draw() {
         for(let jj=0;jj<h;jj++) {
             if(dand[j][jj] != null) {
                 if(dand[j][jj].alive == true) { 
-                    teamscores[dand[j][jj].teamid].score =  teamscores[dand[j][jj].teamid].score + dand[j][jj].health;
+                    teamscores[dand[j][jj].teamid].score++;
                     if(age(dand[j][jj])) {
                         cellsToKill.push([j,jj]);
                         continue;
                     }
                     dand[j][jj].age++;
-                /*    if(dand[j][jj].health <= 0) {
-                        cellsToKill.push([j, jj, dand[j][jj]]);
+                   if(dand[j][jj].health <= 0) {
+                        cellsToKill.push([j, jj]);
                         continue;
-                    } */
+                    } 
 
                     for(let i = 0; i!=coor.length; i++) { 
                         if(dand[j + coor[i][0]][jj + coor[i][1]] != null) {
@@ -86,6 +86,7 @@ function draw() {
                                 continue;
                             } else if (dand[j + coor[i][0]][jj + coor[i][1]].team != dand[j][jj].team) { 
                                 cellsToFight.push([[dand[j + coor[i][0]][jj + coor[i][1]], j + coor[i][0], jj + coor[i][1]],[dand[j][jj], j, jj]]);
+                                break;
                             }
                         } 
                     }
@@ -129,7 +130,7 @@ function bringAliveTeam(team) {
     })
 
         document.getElementById("scoreboard").innerHTML = 
-        "<h4 id='team"+team+"'>Team "+team+": "+ teamscores[team].score + "</h4>" + document.getElementById("scoreboard").innerHTML;
+        "<div class='subscore'><div class='square' style='background-color: rgb("+teamcolour+"');></div><h4 id='team"+team+"'>Team "+team+": "+ teamscores[team].score + "</h4> </div>" + document.getElementById("scoreboard").innerHTML;
         setInterval(() => {
         document.getElementById("team"+team).innerHTML =      "Team "+team+": "+ teamscores[team].score;
     
@@ -146,7 +147,7 @@ function bringAliveTeam(team) {
 
 function bringAliveManual(w, h,parent) {
     let chanceOfBirth = Math.random();
-    if(chanceOfBirth > 0.3) {
+    if(chanceOfBirth > 0.6) {
     set(w, h,[parent.team[0],parent.team[1], parent.team[2],255]);
     const child = mutate(parent);
     dand[w][h] = child;
@@ -158,11 +159,25 @@ function killManual(w, h) {
     dand[w][h].alive = false;
     dand[w][h].team = [];
     dand[w][h].health = 0;
+    teamscores[dand[w][h].teamid].score--;
+}
+
+function killEnemy(w, h, parent) {
+    set(w, h,parent.team);
+    dand[w][h].alive = true;
+    dand[w][h].team = parent.team;
+    dand[w][h].health = parent.health
+    dand[w][h].age = 0;
+    dand[w][h].teamid = parent.teamid;
 }
 
 function age(cell) {
     const cellDeathDice = Math.floor(Math.random() * ((cell.age - 1) + 1) + 1);
-    if(cellDeathDice > 25) {
+    const suddenDeath = Math.random(Math.random() * ((12 - 1) + 1) + 1);
+    if(cellDeathDice > 50) {
+        return true;
+    }
+    else if (suddenDeath == 12) {
         return true;
     }
     else {
@@ -177,12 +192,15 @@ function fight(cells) {
     const cell2 = cells[1][0];
     const cell2w = cells[1][1];
     const cell2h = cells[1][2];
-    const cell1Dice = Math.floor(Math.random() * ((6 - 1) + 1) + 1);
-    const cell2Dice = Math.floor(Math.random() * ((6 - 1) + 1) + 1);
-    if(cell1.heath/100 + cell1Dice < cell2Dice + cell2.health/100) {
-        killManual(cell2w, cell2h)
+    const cell1Dice = Math.floor(Math.random() * ((100 - 1) + 1) + 1);
+    const cell2Dice = Math.floor(Math.random() * ((100 - 1) + 1) + 1);
+    if((cell1.health/100 + cell1Dice + teamscores[cells[1][0].teamid].score/1000000) < (cell2Dice + cell2.health/100  + teamscores[cells[0][0].teamid].score/1000000)) {
+        teamscores[cell1.teamid].score++;
+        killEnemy(cell2w, cell2h, cell1);
     } else {
-        killManual(cell1w, cell1h);
+        teamscores[cell2.teamid].score++;
+        killEnemy(cell1w, cell1h, cell2);
+       
     }
 }
 
@@ -196,12 +214,14 @@ function mutate(parent) {
        };
     child.team = parent.team;
     child.alive = true;
+    
     let healthMutation = Math.random();
     switch(true) {
-        case healthMutation > 0.9: child.health = parent.health + 200
-        break;
-      //  case healthMutation < 0.3: child.health = parent.health - 100;
-      //  break;
+     case healthMutation > 0.99: child.health = parent.health + 100
+       break;
+       default: child.health = parent.health + 0;
+    //    case healthMutation < 0.1: child.health = parent.health - 10;
+    //    break;
     }
     return child;
 }
