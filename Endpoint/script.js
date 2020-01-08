393770b861d78bbb7703687513e49f4a3d444ea2
Tom <tomwilliamfranklin@gmail.com>
2020-01-08 20:19:32 +0000

    removed unused ui library

--- Endpoint/script.js
+++ Endpoint/script.js
@@ -247,19 +247,8 @@ function mouseClicked() {
 }
 
 var FizzyText = function() {
-    this.message = 'dat.gui';
-    this.speed = 0.8;
-    this.displayOutline = false;
-
-    // Define render logic ...
-  };
+};
 
 $(document).ready(function() {
-  var text = new FizzyText();
-  var gui = new dat.GUI();
-  gui.add(text, 'message');
-  gui.add(text, 'speed', -5, 5);
-  gui.add(text, 'displayOutline');
-  gui.add(text, 'explode');
     
 });
