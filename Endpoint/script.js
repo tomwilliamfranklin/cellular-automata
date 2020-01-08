2210a1c3add83d51c2e160ecf8ca3110f6c42d69
Tom <tomwilliamfranklin@gmail.com>
2020-01-08 21:18:31 +0000

    UI overhaul. You can now select colour explosion for crazy affects, you can now select rules for your desired playing field.

--- Endpoint/script.js
+++ Endpoint/script.js
@@ -1,64 +1,74 @@
 //Made with ♥ by Tom Franklin
 
-const w=540; //Size of Grid, recommend equal height and width
-const h=360;
+const w=810; //Size of Grid, recommend equal height and width
+const h=540;
 let frames = 30; 
 let teams = 20; 
 //green = 4,255,0
 //blue = 26,0,255
 let img;
 let loading = false;
-function preload() {
-  img = loadImage('map.jpg');
-}
 let teamscores = [];
-dand = new Array(w+1);
-greenLand = [];
-function setup() {
-    document.getElementById("scoreboard").innerHTML = "";
-    loading = true;
-    cells = [];
-    createCanvas(w, h);
-    background(200);
-    image(img, 0, 0, width, height);
-    
-    nextposw = 0;
-    nextposh = 0;
-    frameRate(frames); // Attempt to refresh at starting FPS
+let dand = new Array(w+1);
+let greenLand = [];
+let p5canvas = null;
 
-    for(let i = 0; i<w;i++) {
-        dand[i] = new Array(h);
+
+let settings = {
+    rule: 1,
+    explosions: false,
+}
+
+let sketch = function(p) {
+    p.preload = function() {
+        img = p.loadImage('map.jpg');
     }
-    for(let j = 0; j<w;j++) {
-        for(let jj=0; jj<h;jj++) {
-            if(get(j,jj)[0] === 4 && get(j,jj)[1] === 255 && get(j,jj)[2] === 0) {     
-                 dand[j][jj] = {
-                     alive:false,
-                     team:[],
-                     health:0,
-                     age: 0,
-                     teamid:0,
-                    };
-                    dand[j][jj].health = 0;
-                 greenLand.push([j,jj]);
+    
+    p.setup = function() {
+        document.getElementById("scoreboard").innerHTML = "";
+        loading = true;
+        cells = [];
+        p.createCanvas(w, h);
+        p.image(img, 0, 0, w, h);
+        nextposw = 0;
+        nextposh = 0;
+        p.frameRate(frames); // Attempt to refresh at starting FPS
+
+        for(let i = 0; i<w;i++) {
+            dand[i] = new Array(h);
+        }
+
+        for(let j = 0; j<w;j++) {
+            for(let jj=0; jj<h;jj++) {
+                if(p.get(j,jj)[0] === 4 && p.get(j,jj)[1] === 255 && p.get(j,jj)[2] === 0) {  
+                    dand[j][jj] = {
+                        alive:false,
+                        team:[],
+                        health:0,
+                        age: 0,
+                        teamid:0,
+                        };
+                        dand[j][jj].health = 0;
+                    greenLand.push([j,jj]);
+                }
             }
         }
+        $("#defaultCanvas0").css({ 'height': "720px" });
+        $("#defaultCanvas0").css({ 'width': "1080px" });
+        for(let i = 0; i<teams; i++) {
+            bringAliveTeam(i, p); //random team colours
+        }
+        loading = false;
     }
 
-    $("#defaultCanvas0").css({ 'height': "720px" });
-    $("#defaultCanvas0").css({ 'width': "1080px" });
-    for(let i = 0; i<teams; i++) {
-        bringAliveTeam(i); //random team colours
-    }
-    loading = false;
-}
-
+    
 const coor = [[-1, -1], [-1, 0], [-1, +1],
 [ 0, -1],          [ 0, +1],
 [+1, -1], [+1, 0], [+1, +1]];
 
 dunk = 0;
-function draw() {
+p.draw = function() {
+    p5canvas = p;
     let leng = dand.length;
     let cellsToLive = [];
     let cellsToKill = [];
@@ -84,7 +94,7 @@ function draw() {
                             if(dand[j + coor[i][0]][jj + coor[i][1]].alive == false) {                              
                                 cellsToLive.push([j + coor[i][0], jj + coor[i][1], dand[j][jj]]);
                                 continue;
-                            } else if (dand[j + coor[i][0]][jj + coor[i][1]].team != dand[j][jj].team) { 
+                            } else if (dand[j + coor[i][0]][jj + coor[i][1]].teamid != dand[j][jj].teamid) { 
                                 cellsToFight.push([[dand[j + coor[i][0]][jj + coor[i][1]], j + coor[i][0], jj + coor[i][1]],[dand[j][jj], j, jj]]);
                                 break;
                             }
@@ -101,25 +111,26 @@ function draw() {
          }
         }
     }
-    for(var e = 0; e < cellsToLive.length; e++) {
-        bringAliveManual(cellsToLive[e][0], cellsToLive[e][1], cellsToLive[e][2]);
-    }
-    
-    for(var e = 0; e < cellsToFight.length; e++) {
-         fight(cellsToFight[e]);
-    }
+        for(var e = 0; e < cellsToLive.length; e++) {
+            bringAliveManual(cellsToLive[e][0], cellsToLive[e][1], cellsToLive[e][2], p);
+        }
+        
+        for(var e = 0; e < cellsToFight.length; e++) {
+            fight(cellsToFight[e], p);
+        }
 
-    for(var e = 0; e < cellsToKill.length; e++) {
-        killManual(cellsToKill[e][0], cellsToKill[e][1]);
-    }
+        for(var e = 0; e < cellsToKill.length; e++) {
+            killManual(cellsToKill[e][0], cellsToKill[e][1], p);
+        }
 
-    updatePixels();
+    p.updatePixels();
+    }
 }
 
-function bringAliveTeam(team) {
+function bringAliveTeam(team, p) {
     let randomw =  (Math.floor((Math.random() * greenLand.length)));
     let teamcolour = [Math.random() * 255,Math.random() * 255, Math.random() * 255,255];
-    set(greenLand[randomw][0], greenLand[randomw][1], teamcolour);
+    p.set(greenLand[randomw][0], greenLand[randomw][1], teamcolour);
     dand[greenLand[randomw][0]][greenLand[randomw][1]].alive = true;
     dand[greenLand[randomw][0]][greenLand[randomw][1]].team = teamcolour;
     dand[greenLand[randomw][0]][greenLand[randomw][1]].health = 100;
@@ -145,28 +156,33 @@ function bringAliveTeam(team) {
     land.splice(random, 1); */
 }
 
-function bringAliveManual(w, h,parent) {
+function bringAliveManual(w, h,parent, p) {
     let chanceOfBirth = Math.random();
     if(chanceOfBirth > 0.6) {
-    set(w, h,[parent.team[0],parent.team[1], parent.team[2],255]);
+    p.set(w, h,[parent.team[0],parent.team[1], parent.team[2],255]);
     const child = mutate(parent);
     dand[w][h] = child;
     }
 }
 
-function killManual(w, h) {
-    set(w, h,[4,255,0,255]);
+function killManual(w, h, p) {
+    p.set(w, h,[4,255,0,255]);
     dand[w][h].alive = false;
     dand[w][h].team = [];
     dand[w][h].health = 0;
     teamscores[dand[w][h].teamid].score--;
 }
 
-function killEnemy(w, h, parent) {
-    set(w, h,parent.team);
+function killEnemy(w, h, parent, p) {
+    p.set(w, h,parent.team);
     dand[w][h].alive = true;
     dand[w][h].team = parent.team;
-    dand[w][h].health = parent.health
+    if(settings.rule === 3) {
+        dand[w][h].health = parent.health - 10;
+    } else {
+        dand[w][h].health = parent.health;
+    }
+
     dand[w][h].age = 0;
     dand[w][h].teamid = parent.teamid;
 }
@@ -185,7 +201,7 @@ function age(cell) {
     }
 }
 
-function fight(cells) {
+function fight(cells, p) {
     const cell1 = cells[0][0];
     const cell1w = cells[0][1];
     const cell1h= cells[0][2];
@@ -194,15 +210,48 @@ function fight(cells) {
     const cell2h = cells[1][2];
     const cell1Dice = Math.floor(Math.random() * ((100 - 1) + 1) + 1);
     const cell2Dice = Math.floor(Math.random() * ((100 - 1) + 1) + 1);
-    if((cell1.health/100 + cell1Dice + teamscores[cells[1][0].teamid].score/1000000) < (cell2Dice + cell2.health/100  + teamscores[cells[0][0].teamid].score/1000000)) {
-        teamscores[cell1.teamid].score++;
-        killEnemy(cell2w, cell2h, cell1);
-    } else {
-        teamscores[cell2.teamid].score++;
-        killEnemy(cell1w, cell1h, cell2);
-       
+    switch(settings.rule) {
+        case 1: //Easy wins
+            if((cell1.health/100 + cell1Dice + teamscores[cells[1][0].teamid].score/1000000) < (cell2Dice + cell2.health/100  + teamscores[cells[0][0].teamid].score/1000000)) {
+                teamscores[cell1.teamid].score++;
+                killEnemy(cell2w, cell2h, cell1, p);
+            } else {
+                teamscores[cell2.teamid].score++;
+                killEnemy(cell1w, cell1h, cell2, p);
+            
+            } 
+        break;
+        
+        case 2: 
+            if((cell1.health/100 + cell1Dice) < (cell2Dice + cell2.health/100)) {
+                teamscores[cell1.teamid].score++;
+                let child = mutate(cell1);
+                killEnemy(cell2w, cell2h, child, p);
+            } else {
+                let child = mutate(cell2);
+                killEnemy(cell1w, cell1h, child, p);
+               
+            }
+            break;
+        case 3: 
+            if((cell1.health/100 + cell1Dice + teamscores[cells[1][0].teamid].score/1000000) < (cell2Dice + cell2.health/100  + teamscores[cells[0][0].teamid].score/1000000)) {
+                teamscores[cell1.teamid].score++;
+                killEnemy(cell2w, cell2h, cell1, p);
+            } else {
+                teamscores[cell2.teamid].score++;
+                killEnemy(cell1w, cell1h, cell2, p);
+            
+            } 
+        break;
+        case 4: 
+        break;
+
     }
+
 }
+
+
+let direction = true;
 
 function mutate(parent) {
     let child = {
@@ -217,8 +266,21 @@ function mutate(parent) {
     
     let healthMutation = Math.random();
     switch(true) {
-     case healthMutation > 0.99: child.health = parent.health + 100
+     case healthMutation > 0.99: {
+            child.health = parent.health + 100;
+            if(settings.explosions) {
+                if(child.team[0] > 255) {
+
+                child.team[0] = 200;  
+                } else {
+                    child.team[0] = child.team[0] + 0.10;
+                }
+            }
+        }
        break;
+       case healthMutation > 0.70: {
+           child.age = child.age - 10;
+       }
        default: child.health = parent.health + 0;
     //    case healthMutation < 0.1: child.health = parent.health - 10;
     //    break;
@@ -228,17 +290,26 @@ function mutate(parent) {
 
 function changeFrameRate() {
     frames = parseInt(document.getElementById('frames').value);
-    frameRate(frames);
+    p5canvas.frameRate(frames);
 }
 
 function setTeams() {
     teams = parseInt(document.getElementById('teams').value);
 }
 
+function setExplosions() {
+    settings.explosions = document.getElementById('explosions').checked;
+}
+
+function setRule() {
+    var selector = document.getElementById("ruleSelect");
+    settings.rule = parseInt(selector.options[selector.selectedIndex].value);
+}
+
 
 function resetFunc() {
-    clear();
-    setup();
+    p5canvas.clear();
+    p5canvas.setup();
 }
 
 
@@ -246,9 +317,6 @@ function mouseClicked() {
 
 }
 
-var FizzyText = function() {
-};
-
 $(document).ready(function() {
-    
+    new p5(sketch, 'container');
 });
